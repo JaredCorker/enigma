e8e37acc85df5f343060996dbfe957e248d0f1d5
Jared Corker <jared.corker@gmail.com>
2021-11-14 14:41:11 +1100

    project setup

---
+++ enigma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
